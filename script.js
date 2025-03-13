53a0726aa4bb7e59a48f81ca4555efbd9508822a
DrewBagelz <152040968+DrewBagelz@users.noreply.github.com>
2025-03-12 20:58:18 -0400

    Begin working on Gameboard module
    Code is based heavily on Alex Younger's ConnectFourConsole Replit linked
    in his article, "Building A House From The Inside Out" featured on
    ayweb.dev/blog.

---
+++ script.js
@@ -0,0 +1,44 @@
+function Gameboard() {
+	const rows = 3;
+	const columns = 3;
+	const board = [];
+
+	for (let i = 0; i < rows; i++) {
+		board[i] = [];
+		for (let j = 0; j < columns; j++) {
+			board[i].push(cell());
+		}
+	}
+
+	const cell = () => {
+		let value = "empty";
+		const addMarker = (player) => {
+			value = player;
+		};
+		const getValue = () => value;
+		return {
+			addMarker,
+			getValue,
+		};
+	};
+
+	const getBoard = () => board;
+
+	const placeMarker = (row, column, player) => {
+		if (getValue === "") {
+			board[row][column].addMarker(player);
+		} else {
+			return;
+		}
+	};
+
+	// DELETE after building UI
+	const printBoard = () => {
+		const boardWithCellValues = board.map((row) =>
+			row.map((cell) => cell.getValue())
+		);
+		console.log(boardWithCellValues);
+	};
+
+	return { getBoard, placeMarker, printBoard };
+}
